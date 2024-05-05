fda859f1bdea3e3fd591456680f8816fc58c294c
fyihany <116070250+fyihany@users.noreply.github.com>
2024-05-05 17:41:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const orderForm = document.getElementById("order-form");
+// const orderForm = document.getElementById("order-form");
 
-orderForm.onsubmit = function(e) {
-    e.preventDefault()
+// orderForm.onsubmit = function(e) {
+//     e.preventDefault()
 
-    let formData = new FormData(orderForm)
-    for (let pair of formData.entries()) {
-        console.log(pair[0]+ ', ' + pair[1]); 
-    }
+//     let formData = new FormData(orderForm)
+//     for (let pair of formData.entries()) {
+//         console.log(pair[0]+ ', ' + pair[1]); 
+//     }
 
-    let appScriptURL = "https://script.google.com/macros/s/AKfycbxkRDt0KL4b0_Fqt885bcjWQj8AFoRD-jVwdT1qecHeZMSzs4E1Dz00KgbjIpTg-Qg6/exec"
+//     let appScriptURL = "https://script.google.com/macros/s/AKfycbxkRDt0KL4b0_Fqt885bcjWQj8AFoRD-jVwdT1qecHeZMSzs4E1Dz00KgbjIpTg-Qg6/exec"
 
-    fetch(appScriptURL, {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.text())
-    .then(data => {
-        console.log("Succes: ", data)
-    })
-    .catch("Error: ", error)
-}
+//     fetch(appScriptURL, {
+//         method: 'POST',
+//         body: formData
+//     })
+//     .then(response => response.text())
+//     .then(data => {
+//         console.log("Succes: ", data)
+//     })
+//     .catch("Error: ", error)
+// }
